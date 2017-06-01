4c8ce274355f3b20fe9e368e042d9a59e164befd
daniel <danielnilssoncole@gmail.com>
2017-06-01 06:34:21 -0400

    search

--- twitter_search.py
+++ twitter_search.py
@@ -3,6 +3,7 @@ from keys import api_key, api_secret, access_token, access_token_secret
 from dateutil import parser
 import json
 import datetime
+import pytz
 
 ACCESS_TOKEN = access_token
 ACCESS_SECRET = access_token_secret
@@ -11,21 +12,29 @@ CONSUMER_SECRET = api_secret
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
-# search_term = raw_input('Enter your search term: ')
+search_term = raw_input('Enter your search term: ')
+print ''
 
 twitter = Twitter(auth=oauth)
-results = twitter.search.tweets(q='nasa', result_type='popular', lang='en', count=2)
+results = twitter.search.tweets(q=search_term, result_type='popular', lang='en', count=10)
 parsed = json.loads(json.dumps(results))
 statuses = parsed['statuses']
 
 # print json.dumps(statuses, indent=4)
 
+dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
+dt_east = dt_utcnow.astimezone(pytz.timezone('US/Eastern'))
+dt_east_str = dt_east.strftime('%m/%d/%Y %I:%M %p')
+print dt_east_str
+
 for status in statuses:
     screen_name = status['user']['screen_name']
     ct_str = status['created_at']
     parsed_date = parser.parse(ct_str)
-    created_time = parsed_date.strftime('%m/%d/%Y %I:%M:%S %p')
+    created_time = parsed_date.strftime('%m/%d/%Y %I:%M %p')
     text = status['text'].encode('utf-8')
     print '\n@{0}\n{1}\n{2}\n'.format(screen_name, created_time, text)
 
-print json.dumps(statuses[1], indent=4)
+with open('search.txt', 'w') as f:
+    f.write(search_term + '\n')
+    f.write(dt_east_str)
